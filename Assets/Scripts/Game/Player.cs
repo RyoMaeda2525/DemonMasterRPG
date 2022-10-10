89092206c65a21c2d5620519a4abd62a6a652c6e
RyoMaeda2525 <maetyuryu@gmail.com>
2022-10-10 23:53:27 +0900

    移動以外のプレイヤーの行動をまとめたPlayerActionを作成した

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Player : SingletonMonoBehaviour<Player>
 {
@@ -14,11 +15,8 @@ public class Player : SingletonMonoBehaviour<Player>
 
     public List<Item> _items = new List<Item>();
 
-    /// <summary>作戦指示中の停止用</summary>
-    private bool _actionBool = false;
-
     /// <summary>現在設定している作戦リスト</summary>
-    private TacticsList[] _tacticsArray;
+    public TacticsList[] _tacticsArray;
 
     /// <summary>現在出している作戦</summary>
     private TacticsList _tactics;
@@ -26,7 +24,7 @@ public class Player : SingletonMonoBehaviour<Player>
     /// <summary>戦闘範囲内にいる敵のリスト</summary>
     public List<EnemyMonsterMove> _emmList;
 
-    void SetTactics(int[] tacticsNumber)
+    private void SetTactics(int[] tacticsNumber)
     {
         _tacticsArray = TacticsManager.Instance.TacticsSet(tacticsNumber);
     }
@@ -37,53 +35,12 @@ public class Player : SingletonMonoBehaviour<Player>
         SetTactics(_tacticsSetArray);
     }
 
-    void FixedUpdate()
+    public void ConductTactics(int i)
     {
-        //foreach(var pms in _pms) 
-        //{
-        //    if (!pms.gameObject.activeSelf) { _pms.Remove(pms); }
-        //}
-
-        float h = Input.GetAxis("HorizontalKey");              // 矢印キーの水平軸をhで定義
-        float v = Input.GetAxis("VerticalKey");                // 矢印キーの垂直軸をvで定義
-
-        if (Input.GetButton("Jump")) 
-        {
-            if (!_actionBool && h != 0 || !_actionBool && v != 0)
-            {
-                UseItems(h, v);
-            }
-        }
-        else if (!_actionBool && h != 0 || !_actionBool && v != 0)
-        {
-            ChangeTactics(h, v);
-        }
-    }
-
-    /// <summary>
-    /// 作戦を指示する
-    /// </summary>
-    /// <param name="h">横</param>
-    /// <param name="v">縦</param>
-    void ChangeTactics(float h, float v)
-    {
-        StartCoroutine(ActionStop(3.0f));
-
-        int i = -1;
-        if (h > 0) { i = 0; } //右
-        else if (v > 0) { i = 1; }//下
-        else if (h < 0) { i = 2; }//左
-        else if (v < 0) { i = 3; }//上
-
-        //入力がないとき
-        if (i == -1) { Debug.Log("Error01"); return; }
-
-        //手持ちのモンスターがいないとき
-        if (_pms.Count == 0) { Debug.Log("Error02"); return; }
+        _tactics = _tacticsArray[i];
 
         Debug.Log($"{_tacticsArray[i].tactics_id} {_tacticsArray[i].tactics_name} {_tacticsArray[i].tactics_info} {_tacticsArray[i].tactics_type}");
 
-        _tactics = _tacticsArray[i];
 
         foreach (var monster in _pms)
         {
@@ -95,22 +52,8 @@ public class Player : SingletonMonoBehaviour<Player>
         }
     }
 
-    private void UseItems(float h, float v) 
+    public void UseItems(int i) 
     {
-        StartCoroutine(ActionStop(3.0f));
-
-        int i = -1;
-        if (h > 0) { i = 0; } //右
-        else if (v > 0) { i = 1; }//下
-        else if (h < 0) { i = 2; }//左
-        else if (v < 0) { i = 3; }//上
-
-        //入力がないとき
-        if (i == -1) { Debug.Log("Error01"); return; }
-
-        //手持ちのモンスターがいないとき
-        if (_pms.Count == 0) { Debug.Log("Error02"); return; }
-
         Debug.Log($"{_items[i].name} {_items[i].infomation} {_items[i].type}");
 
         UseItem.Instance.UseItemEffect(_items[i]);
@@ -139,12 +82,5 @@ public class Player : SingletonMonoBehaviour<Player>
         {
             _emmList.Remove(other.GetComponent<EnemyMonsterMove>());
         }
-    }
-
-    private IEnumerator ActionStop(float waitTime)
-    {
-        _actionBool = true;
-        yield return new WaitForSeconds(waitTime);
-        _actionBool = false;
     }
 }
---
+++ Assets/Scripts/Game/PlayerAction.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    [SerializeField]
+    Player _player = null;
+
+    /// <summary>作戦指示中の停止用</summary>
+    private bool _actionBool = false;
+
+    void FixedUpdate()
+    {
+
+        float h = Input.GetAxis("HorizontalKey");              // 矢印キーの水平軸をhで定義
+        float v = Input.GetAxis("VerticalKey");                // 矢印キーの垂直軸をvで定義
+
+        if (Input.GetButton("Jump"))
+        {
+            if (!_actionBool && h != 0 || !_actionBool && v != 0)
+            {
+                UseItems(h, v);
+            }
+        }
+        else if (!_actionBool && h != 0 || !_actionBool && v != 0)
+        {
+            ChangeTactics(h, v);
+        }
+    }
+
+    /// <summary>
+    /// 作戦を指示する
+    /// </summary>
+    /// <param name="h">横</param>
+    /// <param name="v">縦</param>
+    void ChangeTactics(float h, float v)
+    {
+
+        int i = -1;
+        if (h > 0) { i = 0; } //右
+        else if (v > 0) { i = 1; }//下
+        else if (h < 0) { i = 2; }//左
+        else if (v < 0) { i = 3; }//上
+
+        //入力がないとき
+        if (i == -1) { Debug.Log("Error01"); return; }
+
+        //手持ちのモンスターがいないとき
+        if (_player._pms.Count == 0) { Debug.Log("Error02"); return; }
+
+        StartCoroutine(ActionStop(3.0f));
+
+        _player.ConductTactics(i);
+    }
+
+    private void UseItems(float h, float v)
+    {
+
+        int i = -1;
+        if (h > 0) { i = 0; } //右
+        else if (v > 0) { i = 1; }//下
+        else if (h < 0) { i = 2; }//左
+        else if (v < 0) { i = 3; }//上
+
+        //入力がないとき
+        if (i == -1) { Debug.Log("Error01"); return; }
+
+        //手持ちのモンスターがいないとき
+        if (_player._pms.Count == 0) { Debug.Log("Error02"); return; }
+
+        StartCoroutine(ActionStop(3.0f));
+
+        _player.UseItems(i);
+    }
+
+    private IEnumerator ActionStop(float waitTime)
+    {
+        _actionBool = true;
+        yield return new WaitForSeconds(waitTime);
+        _actionBool = false;
+    }
+}
