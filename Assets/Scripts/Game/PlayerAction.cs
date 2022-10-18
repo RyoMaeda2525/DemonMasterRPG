3113ba16bf7052f9078edda698cf1ce6c870145a
RyoMaeda2525 <maetyuryu@gmail.com>
2022-10-18 13:44:05 +0900

    アイテムスロットと作戦スロットの入れ替えを修正

--- Assets/Scripts/Game/PlayerAction.cs
+++ Assets/Scripts/Game/PlayerAction.cs
@@ -10,35 +10,56 @@ public class PlayerAction : MonoBehaviour
     /// <summary>作戦指示中の停止用</summary>
     private bool _actionBool = false;
 
+    /// <summary> アイテムスロットを表示しているか判定する</summary>
+    bool _itemSlotBool = false;
+
     void FixedUpdate()
     {
-
-        float h = Input.GetAxis("HorizontalKey");              // 矢印キーの水平軸をhで定義
-        float v = Input.GetAxis("VerticalKey");                // 矢印キーの垂直軸をvで定義
         float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
-        
-        if (scrollWheel > 0) { TacticSlot.Instance.WheelUp(); }
-        else if (scrollWheel < 0) { TacticSlot.Instance.WheelDown(); }
-
 
         if (Input.GetButton("Jump"))
         {
-            if (!_actionBool && h != 0 || !_actionBool && v != 0)
-            {
-                UseItems(h, v);
-            }
+            SlotChange(true);
         }
-        else if (Input.GetKeyDown(KeyCode.F))
+        else
         {
-            ChangeTactics();
+            SlotChange(false);
+        }
+
+        if (!_itemSlotBool)
+        {
+            if (!_actionBool && Input.GetKeyDown(KeyCode.F))
+            {
+                ChangeTactics();
+            }
+            else if (scrollWheel > 0) { TacticSlot.Instance.WheelUp(); }
+            else if (scrollWheel < 0) { TacticSlot.Instance.WheelDown(); }
+        }
+        else
+        {
+            if (!_actionBool && Input.GetKeyDown(KeyCode.F))
+            {
+                UseItems();
+            }
+            else if (scrollWheel > 0) { ItemSlot.Instance.WheelUp(); }
+            else if (scrollWheel < 0) { ItemSlot.Instance.WheelDown(); }
+        }
+
+    }
+
+
+    /// <summary>表示するスロットを切り替える</summary>
+    private void SlotChange(bool itemSlotactive)
+    {
+        if (_itemSlotBool != itemSlotactive)
+        {
+            TacticSlot.Instance.TacticsSlotActiveChange();
+            ItemSlot.Instance.ItemSlotActiveChange();
+            _itemSlotBool = itemSlotactive;
         }
     }
 
-    /// <summary>
-    /// 作戦を指示する
-    /// </summary>
-    /// <param name="h">横</param>
-    /// <param name="v">縦</param>
+    /// <summary>作戦を指示する</summary>
     void ChangeTactics()
     {
         int index = TacticSlot.Instance._selectIndex;
@@ -48,24 +69,15 @@ public class PlayerAction : MonoBehaviour
         _player.ConductTactics(index);
     }
 
-    private void UseItems(float h, float v)
+    /// <summary>手持ちのアイテムを使用する </summary>
+    private void UseItems()
     {
 
-        int i = -1;
-        if (h > 0) { i = 0; } //右
-        else if (v > 0) { i = 1; }//下
-        else if (h < 0) { i = 2; }//左
-        else if (v < 0) { i = 3; }//上
-
-        //入力がないとき
-        if (i == -1) { Debug.Log("Error01"); return; }
-
-        //手持ちのモンスターがいないとき
-        if (_player._pms.Count == 0) { Debug.Log("Error02"); return; }
+        int index = ItemSlot.Instance._selectIndex;
 
         StartCoroutine(ActionStop(3.0f));
 
-        _player.UseItems(i);
+        _player.UseItems(index);
     }
 
     private IEnumerator ActionStop(float waitTime)
