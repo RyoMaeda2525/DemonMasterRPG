b3b327ddd7316469dc18f2160dd4f8def8de06b4
RyoMaeda2525 <maetyuryu@gmail.com>
2023-01-17 15:07:46 +0900

    [update]Navmeshでランダムに移動するノードを作成

--- Assets/Scripts/Nodes/Action/Move.cs
+++ Assets/Scripts/Nodes/Action/Move.cs
@@ -9,33 +9,57 @@ namespace MonsterTree
     [Serializable]
     public class RandomMove : IBehavior
     {
-        [Tooltip("行動範囲の中心点"), SerializeField]
-        public Vector3 startPosition = default;
         [Tooltip("行動範囲の半径"), SerializeField]
         public float Actionradius = 3;
         [Tooltip("行動の制限距離"), SerializeField]
         public float ActionDistance = 20f;
 
+        /// <summary>行動範囲の中心点</summary>
+        private Vector3 _startPosition = Vector3.zero;
+
+        Vector3 randomPos = Vector3.zero;
+
         NavMeshAgent _nav;
 
         public Result Action(Environment env)
         {
-            NavMeshHit navMeshHit;
+            if (env.Visit(this))
+            {
+                if (_startPosition == Vector3.zero)
+                {
+                    _startPosition = env.mySelf.transform.position;
+                    _nav = env.mySelf.GetComponent<NavMeshAgent>();
+                }
 
-            Vector3 randomPos = new Vector3(UnityEngine.Random.Range(startPosition.x - Actionradius, startPosition.x + Actionradius), 0,
-                                                            UnityEngine.Random.Range(startPosition.z - Actionradius, startPosition.z + Actionradius));
-            NavMesh.SamplePosition(randomPos, out navMeshHit, 10, 1);
-            _nav.SetDestination(navMeshHit.position);
+                randomPos = new Vector3(UnityEngine.Random.Range(_startPosition.x - Actionradius, _startPosition.x + Actionradius), 0,
+                                                                UnityEngine.Random.Range(_startPosition.z - Actionradius, _startPosition.z + Actionradius));
+
+                NavMeshHit navMeshHit;
+
+                NavMesh.SamplePosition(randomPos, out navMeshHit, 10, 1);
+
+                _nav.destination = navMeshHit.position;
+            }
+
+            if ((randomPos - env.mySelf.transform.position).magnitude > _nav.stoppingDistance) { return Result.Running; }
 
             return Result.Success;
         }
     }
 
     [Serializable]
-    public class TargetMove 
+    public class TargetMove : IBehavior
     {
+        [SerializeField, Tooltip("追いかける対象")]
+        public GameObject _target;
+
+        [SerializeField, Tooltip("追いかけるのを止める距離")]
+        public Vector3 _targetRange = new Vector3();
+
         public Result Action(Environment evn) 
         {
+
+
             return Result.Success;
         }
     }
